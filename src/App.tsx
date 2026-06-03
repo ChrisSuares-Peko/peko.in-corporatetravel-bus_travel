f2d64b8eed80b0a26385f68626b34a775df6d033
ChrisSuares-Peko <Chris@peko.one>
2026-06-04 01:53:20 +0530

    Bug fix 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import AntdConfig from './antd.config';
 import useCustomNotification from './hooks/useCustomNotification';
 import { useScrollToTop } from './hooks/useScrollToTop';
 // eslint-disable-next-line import/no-cycle
 import Router from './routes/sections';
+
+// TAB_ID is used by auth components (e.g. RegisterStepFive) to identify the
+// current browser tab in BroadcastChannel messages.
+export const TAB_ID = uuidv4();
 
 function App() {
     useScrollToTop();
